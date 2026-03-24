a4a38b87561b08718eb1b741e2d2fe7daff62b67
Nikita <dudinnikita03188@gmail.com>
2026-03-25 00:21:08 +0300

    feat: добавил адреса фронта в cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:8081',
+    origin: ['http://localhost:8081', 'https://dictionary-ai-tau.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
